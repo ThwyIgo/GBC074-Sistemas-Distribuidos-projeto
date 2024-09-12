55f9f0fe0770e332fa118c4e1842907b33897c42
ThwyIgo <thiagopachecorocha@hotmail.com>
2024-09-12 11:13:45 -0300

    cad-server menu interativo

--- src/biblioteca/cad/cad_client.py
+++ src/biblioteca/cad/cad_client.py
@@ -5,30 +5,65 @@ import grpc
 from biblioteca import lib
 from biblioteca.gRPC import cadastro_pb2, cadastro_pb2_grpc
 
+stub: cadastro_pb2_grpc.PortalCadastroStub = None
+
 def run():
     porta = int(sys.argv[1])
+    global stub
     stub = connect_stub(porta)
 
     # Loop só para testes
     while True:
-        print("criar")
-        cpf = input("cpf: ")
-        usuario = cadastro_pb2.Usuario(cpf=cpf, nome=input("nome: "))
+        print("""
+Opções:
+0 - Sair
+1 - Criar usuário
+2 - Deletar usuário
+3 - Atualizar usuário
+4 - Buscar usuário
+              """)
+        match int(input("Digite um número: ")):
+            case 0:
+                break
+            case 1:
+                criarUsuario()
+            case 2:
+                deletarUsuario()
+            case 3:
+                atualizarUsuario()
+            case 4:
+                buscarUsuario()
 
+def criarUsuario():
+        print("Criando usuário")
+        usuario = cadastro_pb2.Usuario(cpf=input("cpf: "), nome=input("nome: "))
         status = stub.NovoUsuario(usuario)
-        print(status)
-        print("ler")
-        status = stub.ObtemUsuario(cadastro_pb2.Identificador(id=cpf))
-        print(status)
-        print("atualizar")
-        status = stub.EditaUsuario(cadastro_pb2.Usuario(cpf=cpf, nome=input("novo nome: ")))
-        print(status)
-        print("ler")
-        status = stub.ObtemUsuario(cadastro_pb2.Identificador(id=cpf))
-        print(status)
-        if input("remover? [s]") == "s":
-            status = stub.RemoveUsuario(cadastro_pb2.Identificador(id=cpf))
-            print(status)
+        if (status.status == 0):
+            print("Usuário criado com sucesso!")
+        else:
+            print(status.msg)
+
+def deletarUsuario():
+        print("Deletando usuário")
+        status = stub.RemoveUsuario(cadastro_pb2.Identificador(id=input("cpf: ")))
+        if (status.status == 0):
+            print("Usuário deletado com sucesso!")
+        else:
+             print(status.msg)
+        
+def atualizarUsuario():
+    print("Atualizando usuário")
+    status = stub.EditaUsuario(cadastro_pb2.Usuario(cpf=input("cpf: "), nome=input("novo nome: ")))
+    if (status.status == 0):
+        print("Usuário atualizado com sucesso!")
+    else:
+        print(status.msg)
+
+def buscarUsuario():
+        print("Buscando usuário")
+        usuario = stub.ObtemUsuario(cadastro_pb2.Identificador(id=input("cpf: ")))
+        print("Usuário encontrado:")
+        print(usuario)
 
 def connect_stub(porta: int) -> cadastro_pb2_grpc.PortalCadastroStub:
     channel = grpc.insecure_channel(f"localhost:{porta}")
